09dab49bcc0f684c260d27e2b95780eed58c249d
Marwa Mahmoud <Marwa@dhcp-v065-073.mobile.uci.edu>
2017-12-05 19:59:25 -0800

    changes to user profile

--- client/src/containers/UserProfile.js
+++ client/src/containers/UserProfile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from '../components/Image.jsx';
 import axios from "axios";
+import Auth from "../modules/Auth"
 import { Container, Button, Form, Row, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './userprofile.css';
 
@@ -33,7 +34,7 @@ class UserProfile extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>User Profile</h1>
+      <h3 className="tabTitle">User Profile</h3>
         <form ref='uploadForm' 
           id='uploadForm'
           action = '/upload' 
@@ -47,16 +48,7 @@ class UserProfile extends React.Component {
         </form> 
         <Image />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit,
-          lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi ac dui
-          fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras non risus
-          mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet ac. Sed nec
-          nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-          in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec
-          nec ornare elit. Donec blandit est sed risus feugiat porttitor. Vestibulum
-          molestie hendrerit massa non consequat. Vestibulum vitae lorem tortor. In
-          elementum ultricies tempus. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus.
+          
         </p>
     </div>
     );
