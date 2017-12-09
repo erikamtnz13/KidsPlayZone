e40b2549600cc645f5ecfad57c42ff7159e8a6bf
Erika Martinez <erikamartinez@Erikas-MacBook-Pro.local>
2017-12-08 22:08:48 -0800

    adding chat

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ io.on('connection', function(socket){
  
     
 })
-// server.listen(PORT);
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+server.listen(PORT);
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//   });
